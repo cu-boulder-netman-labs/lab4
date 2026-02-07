d1e8c26919d3c473e6eb01fda24ab233e30b63af
jgpentz <jpentz1@gmail.com>
2026-02-07 15:44:46 -0700

    Finished retrieving running configs

--- getconfig.py
+++ getconfig.py
@@ -1,38 +1,50 @@
 import napalm
 from tools import sshInfo, validateIP, connectivity
 from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime, timezone
 
-def check_device(device):
+def process_config(device):
     host = device['host']
 
+    # Validate IP
     if not validateIP.validate_ip(host):
         return host, "invalid ip"
 
+    # Reachability check
     reachable = connectivity.check_reachability([host])
-    return host, "reachable" if reachable[host] else "unreachable"
+    if not reachable[host]:
+        return host, "unreachable"
+
+    try:
+        driver = napalm.get_network_driver('ios')
+        with driver(
+            hostname=host,
+            username=device['username'],
+            password=device['password'],
+        ) as dev:
+            cfg = dev.get_config()
+
+            # Save in a file based on hostname and ISO8601 format
+            hostname = dev.get_facts()['hostname']
+            ts = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
+            fname = f"configs/{hostname}_{ts}.cfg"
+
+            with open(fname, "w") as f:
+                f.write(cfg["running"])
+
+        return fname
+
+    except Exception as e:
+        return f"{host} error: {e}"
 
 def get_config():
     hosts = sshInfo.load_ssh_info("config/sshInfo.json")
 
-    with ThreadPoolExecutor() as executor:
-        results = executor.map(check_device, hosts)
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        files = executor.map(process_config, hosts)
 
-    for host in hosts:
-        driver = napalm.get_network_driver('ios')
-        device = driver(
-            hostname=host['host'],
-            username=host['username'],
-            password=host['password'],
-        )
+    return list(files)
 
-        device.open()
-
-        cfg = device.get_config()
-        with open(f"{host['host']}_running.cfg", 'w') as f:
-            f.write(cfg["running"])
-        print(cfg)
-
-        device.close()
 
 if __name__ == "__main__":
     get_config()
--- lab4main.py
+++ lab4main.py
@@ -3,6 +3,7 @@ from tools import sshInfo, validateIP, connectivity
 from concurrent.futures import ThreadPoolExecutor
 import threading
 import time
+import getconfig
 
 device_status = {}
 
@@ -11,16 +12,16 @@ def create_app():
 
     @app.route("/")
     def home():
-        return render_template("index.html", devices=device_status)
+        return render_template("index.html")
 
     @app.route("/get_config")
     def get_config():
+        cfg_files = getconfig.get_config()
         return render_template("get_config.html")
 
     @app.route("/ospf_config")
     def ospf_config():
         return render_template("ospf_config.html")
-
 
     @app.route("/diff_config")
     def diff_config():
